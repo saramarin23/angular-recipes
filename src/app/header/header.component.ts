35ecb375b0446bab2d93f3b26a56bc643a0a8264
smarin <sara.marin.pino@accenture.com>
2020-03-26 15:51:12 +0100

    add navigation to the app

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,7 @@
-import { Component, EventEmitter, Output } from "@angular/core";
-// import { EventEmitter } from 'protractor';
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html"
 })
-export class HeaderComponent {
-  @Output() featureSelected = new EventEmitter<string>(); //Output para que el evento se pueda escuchar desde el componente padre (app)
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
-}
+export class HeaderComponent {}
